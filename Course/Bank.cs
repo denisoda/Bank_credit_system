b8f3f5a051a56a7e92e571ff1bd9d5a11bc45183
Ilya Dzeraziak <ilya.by@bk.ru>
2017-11-12 18:09:52 +0300

    Bank class fix

--- Course/Bank.cs
+++ Course/Bank.cs
@@ -4,6 +4,6 @@
     {
         private static string NAME { get { return "DZBank"; } set { NAME = value; } }
         public static string Name = NAME;
-        public static int Amound_of_clients = 0;
+        public static long number_of_clients = DataBase.Last_ID;
     }
 }
